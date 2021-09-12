28028d00a4a63265128c427806f29c323e031095
BrayanMBeltre <brayanmbeltre@gmail.com>
2021-09-11 22:17:01 -0400

    offer-details

--- src/@beca/shared/components/UI/organisms/offert-detail/index.js
+++ src/@beca/shared/components/UI/organisms/offert-detail/index.js
@@ -24,9 +24,9 @@ export default function OfferDetail({ item, message }) {
 
   return (
     <div>
-      <div className="oferta bg-white m-auto grid grid-cols-3 xs:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
-        <div className="ofertaInfo flex flex-col px-7 py-14">
-          <div className="titulo flex items-end pb-5">
+      <div className="oferta bg-white m-auto grid-col-1 grid md:grid-cols-3 lg:grid-cols-3">
+        <div className="ofertaInfo md:flex md:flex-col px-7 py-14">
+          <div className="titulo grid md:flex justify-center md:justify-start md:items-end pb-5">
             <div className="logo w-40">
               <img className="w-full" src={institution_image} alt="" />
             </div>
