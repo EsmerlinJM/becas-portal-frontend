696301e1d3ce794e6387df009920faa4abd8e900
BrayanMBeltre <brayanmbeltre@gmail.com>
2021-09-11 23:07:09 -0400

    notifications

--- src/@beca/shared/components/UI/molecules/notification-card/index.js
+++ src/@beca/shared/components/UI/molecules/notification-card/index.js
@@ -2,13 +2,13 @@ import Group from "../../../../../../img/Group 3908@2x.png";
 
 export default function CardNotification({ item }) {
   return (
-    <span className="w-full flex items-center justify-between bg-white h-16 cursor-pointer px-4 shadow hover:shadow-lg transition-all">
-      <div className="flex items-center space-x-3">
-        <div>
-          <img className="w-10" src={Group} alt="kk" />
-        </div>
-        <div className="text-xs text-gray-400">
-          <p>
+    <span className="grid grid-cols-12 items-center bg-white cursor-pointer p-4 shadow hover:shadow-lg transition-all">
+      <div className="col-span-1 hidden md:flex items-center justify-center">
+        <img className="" src={Group} alt="kk" />
+      </div>
+      <div className="col-span-11 md:col-span-10 flex items-center space-x-3 p-4">
+        <div className="text-sm text-gray-400">
+          <p className="">
             {item.descripcion}
             {/* <span className="font-semibold text-gray-500">Luisaura Jesmin</span>{" "}
             ha completado la evaluación correspondiente a
@@ -19,24 +19,39 @@ export default function CardNotification({ item }) {
             <span className="font-semibold text-gray-500">
               Becas Nacionales
             </span> */}
-            .
           </p>
-          <p className="italic font-thin">{item.creada}</p>
+
+          <div className="flex items-center justify-between mt-4 ">
+            <div className="italic font-thin ">{item.creada}</div>
+            <div className="text-xs italic text-gray-400 cursor-pointer md:hidden">
+              <label className="flex items-center cursor-pointer">
+                {item.read ? "Leída" : "Marcar como leída"}
+                <input
+                  className="mt-0.5 ml-1 w-2.5 h-2.5 appearance-none outline-none border-2 border-blue-400 rounded-xl checked:bg-blue-500 checked:border-transparent cursor-pointer"
+                  type="checkbox"
+                  onChange={() => {}}
+                  checked={item.read}
+                />
+              </label>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col items-end space-y-4">
-        <div>
+      <div className="col-span-1 flex flex-col items-end space-y-4">
+        {/* <div>
           <img
             className="w-3 h-3 cursor-pointer"
             src="img/Close@2x.png"
             alt=""
           />
-        </div>
-        <div className="flex items-center space-x-1 text-xs italic text-gray-400 cursor-pointer">
-          <label className="items-center cursor-pointer hidden md:flex">
-            {item.read ? "Leída" : "Marcar como leída"}
+        </div> */}
+        <div className="text-xs italic text-gray-400 cursor-pointer hidden md:flex">
+          <label className="flex items-center cursor-pointer">
+            <div className="hidden md:block">
+              {item.read ? "Leída" : "Marcar como leída"}
+            </div>
             <input
-              className="mt-0.5 ml-1 w-2.5 h-2.5 appearance-none outline-none border-2 border-blue-400 rounded-xl checked:bg-blue-500 checked:border-transparent"
+              className="mt-0.5 ml-1 w-2.5 h-2.5 appearance-none outline-none border-2 border-blue-400 rounded-xl checked:bg-blue-500 checked:border-transparent cursor-pointer"
               type="checkbox"
               onChange={() => {}}
               checked={item.read}
